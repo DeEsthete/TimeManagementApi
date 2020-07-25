3b9dd2db609ea4edb1623c3756b71ab5c4bd4908
JeLion <djoniqaz@gmail.com>
2020-07-25 14:05:56 +0600

    chore(Schedule) добавил UserId

--- Domain/Entities/Schedule.cs
+++ Domain/Entities/Schedule.cs
@@ -6,6 +6,8 @@ namespace Domain.Entities
 {
     public class Schedule : BaseEntity
     {
+        public long UserId { get; set; }
+        public AppUser User { get; set; }
         public DateTime ScheduleDate { get; set; }
         public IEnumerable<SchedulePeriod> SchedulePeriods { get; set; }
     }
